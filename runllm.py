04e6c16d7e82e84f96506a1d8467bed12b394d10
Nnamaka <nnamaka7@gmail.com>
2024-03-12 18:31:17 +0000

    opor😆

--- runllm.py
+++ runllm.py
@@ -1,6 +1,12 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
+load_dotenv()
 app = FastAPI()
+
+aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
+aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
 @app.get("/")
 def root():
