5995f599e8f73999bb41821ad6fb47f5299f18ba
Nnamaka Dike <54998610+Nnamaka@users.noreply.github.com>
2024-03-12 15:46:35 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
+$ git config --global user.name "Nnamaka"
+$ git config --global user.email "nnamaka7@gmail.com"
+
 sudo apt-get update
 sudo apt install nginx -y
 
 # create python environment
-sudo apt install python3-venv
+sudo apt install python3-venv -y
 python3 -m venv llm-env
 
-
 # activate enviroment
-source llm-env/bin/activate
+source llm-env/bin/activate && pip install -r requirments.txt && pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 
-# install dep
-pip install -r requirments.txt
-
-exit 0
