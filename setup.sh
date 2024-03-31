955efe4162a95f3d4cf7717c28b8a77d38a4ecec
Nnamaka <nnamaka7@gmail.com>
2024-03-31 19:46:28 +0100

    updated setup script to install redis

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# remove this git configuration later
+# REMOVE: 1
 git config --global user.name "Nnamaka"
 git config --global user.email "nnamaka7@gmail.com"
 
@@ -12,10 +14,20 @@ curl -fsSL https://ollama.com/install.sh | sh
 # download gemma 2:2b
 ollama pull gemma:2b
 
-# create python environment
+# create python environment and activate environment
+# REMOVE: 2
 sudo apt install python3-venv -y
 python3 -m venv llm-env
+source llm-env/bin/activate
 
-# activate python environment and install deps
-source llm-env/bin/activate && pip install -r requirments.txt && pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+# install deps
+pip install -r requirments.txt && pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+
+# install redis
+curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+
+sudo apt-get update
+sudo apt-get install redis
 
