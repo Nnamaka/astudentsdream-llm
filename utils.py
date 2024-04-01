b814ff38b2c1911b79a6b3ded1b03624079e9874
Nnamaka <nnamaka7@gmail.com>
2024-04-01 18:10:03 +0100

    server checks

--- utils.py
+++ utils.py
@@ -11,8 +11,11 @@ def add_chat_history(username,user_query, response, r, role='assistant'):
   user_query['timestamp'] = int(time.time())
   llm_response = {'role': role, 'content': f'{response}', 'timestamp': int(time.time())}
 
-  r.zadd(f'user:{username}', {{json.dumps(llm_response): llm_response['timestamp']}})
-  r.zadd(f'user:{username}', {json.dumps(user_query): user_query['timestamp']})
+  user_query_json = json.dumps(user_query)
+  llm_response_json = json.dumps(llm_response)
+  
+  r.zadd(f'user:{username}', {{llm_response_json: llm_response['timestamp']}})
+  r.zadd(f'user:{username}', {user_query_json: user_query['timestamp']})
 
 def add_first_response(username, response, r, role='assistant'):
     llm_response = {'role': role, 'content': f'{response}', 'timestamp': int(time.time())}
